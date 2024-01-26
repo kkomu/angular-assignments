fd6dabd09944a8d4848f01c09cc58fb4412b052f
Kykke <kkomulainen@gmail.com>
2024-01-26 23:20:55 +0200

    Assignment 3 - solution

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  showSecret = true;
+  timestamps = [];
+
+  onClick() {
+    this.showSecret = !this.showSecret;
+    this.timestamps.push(Date.now());
+  }
+
+  isHighlighted(timestamp: string) {
+    return this.timestamps.indexOf(timestamp) >= 4;
+  }
 }

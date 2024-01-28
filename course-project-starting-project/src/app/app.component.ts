8152b0ce455db728e98c059335baee9951c33c12
Kykke <kkomulainen@gmail.com>
2024-01-28 22:12:37 +0200

    Add basic navigation menu functionality

--- course-project-starting-project/src/app/app.component.ts
+++ course-project-starting-project/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MenuOption } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  selectedMenuOption: MenuOption = MenuOption.Recipe;
+
+  onMenuOptionSelected(option: MenuOption) {
+    this.selectedMenuOption = option;
+  }
 }
--- course-project-starting-project/src/app/header/header.component.ts
+++ course-project-starting-project/src/app/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
+export enum MenuOption {
+  Recipe = 'Recipe',
+  ShoppingList = 'ShoppingList',
+}
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  @Output() menuOptionSelected = new EventEmitter<MenuOption>();
 
+  onSelect(option: string) {
+    this.menuOptionSelected.emit(option as MenuOption);
+  }
 }

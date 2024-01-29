89d0bf46dda6bf8d62f19c1c79178229b238e288
Kykke <kkomulainen@gmail.com>
2024-01-29 13:56:36 +0200

    Add events to open recipe details

--- course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-    recipes: Recipe[] = [
-        new Recipe('Test recipe', 'Test description', 'https://www.cookipedia.co.uk/wiki/images/2/2c/Kachumbari_recipe.jpg'),
-        new Recipe('Test recipe 2', 'Test description 2', 'https://www.cookipedia.co.uk/wiki/images/2/2c/Kachumbari_recipe.jpg')
-    ];
+  @Output() selectRecipe = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'Test description',
+      'https://www.cookipedia.co.uk/wiki/images/2/2c/Kachumbari_recipe.jpg'
+    ),
+    new Recipe(
+      'Test recipe 2',
+      'Test description 2',
+      'https://www.cookipedia.co.uk/wiki/images/2/2c/Kachumbari_recipe.jpg'
+    ),
+  ];
+
+  onClickRecipe(recipe: Recipe) {
+    console.log('onClickRecipe', recipe);
+    this.selectRecipe.emit(recipe);
+  }
 }

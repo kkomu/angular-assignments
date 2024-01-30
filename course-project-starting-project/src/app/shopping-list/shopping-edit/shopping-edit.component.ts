ebd189a9fcb45ab7fd133a73e34e5bb9a5f0d02e
Kykke <kkomulainen@gmail.com>
2024-01-30 13:32:56 +0200

    Functionality for adding ingredients

--- course-project-starting-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project-starting-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  ViewChild,
+  Output,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameRef: ElementRef;
+  @ViewChild('amountInput') amountRef: ElementRef;
 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAdd() {
+    const name = this.nameRef.nativeElement.value;
+    const amount = this.amountRef.nativeElement.value;
+
+    //console.log('onAdd', name, amount);
+    this.ingredientAdded.emit(new Ingredient(name, amount));
+  }
 }
--- course-project-starting-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project-starting-project/src/app/shopping-list/shopping-list.component.ts
@@ -4,11 +4,16 @@ import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-    ingredients: Ingredient[] = [
-        new Ingredient('Apple', 5),
-        new Ingredient('Peanut', 100)
-    ];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Peanut', 100),
+  ];
+
+  onIngredientAdded(ingredient: Ingredient) {
+    console.log("I'd like to add this please, ", ingredient);
+    this.ingredients.push(ingredient);
+  }
 }

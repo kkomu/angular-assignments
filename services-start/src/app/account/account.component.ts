997fe39b43c7c1e30310ecdd2a8ac601f901a9bf
Kykke <kkomulainen@gmail.com>
2024-02-06 14:02:57 +0200

    Make use of services

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
@@ -10,20 +11,18 @@ import { LoggingService } from '../logging.service';
 export class AccountComponent {
   @Input() account: { name: string; status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{
-    id: number;
-    newStatus: string;
-  }>();
 
   private loggingService: LoggingService;
+  private accountsService: AccountsService;
 
+  // Alternative way of injecting services
   constructor() {
     this.loggingService = inject(LoggingService);
+    this.accountsService = inject(AccountsService);
   }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
-    //console.log('A server status changed, new status: ' + status);
+    this.accountsService.updateStatus(this.id, status);
     this.loggingService.logStatusChange(status);
   }
 }

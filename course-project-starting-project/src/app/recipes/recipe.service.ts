49495dc2f774fbae5dd18fdf45da23099d3f2a70
Kykke <kkomulainen@gmail.com>
2024-02-09 13:25:51 +0200

    -Show some ingredients in recipe

--- course-project-starting-project/src/app/recipes/recipe.service.ts
+++ course-project-starting-project/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,12 +12,14 @@ export class RecipeService {
     new Recipe(
       'Test recipe',
       'Test description',
-      'https://www.cookipedia.co.uk/wiki/images/2/2c/Kachumbari_recipe.jpg'
+      'https://www.cookipedia.co.uk/wiki/images/2/2c/Kachumbari_recipe.jpg',
+      [new Ingredient('Bun', 1), new Ingredient('Chili', 5)]
     ),
     new Recipe(
       'Test recipe 2',
       'Test description 2',
-      'https://www.cookipedia.co.uk/wiki/images/2/2c/Kachumbari_recipe.jpg'
+      'https://www.cookipedia.co.uk/wiki/images/2/2c/Kachumbari_recipe.jpg',
+      [new Ingredient('Fish', 5), new Ingredient('Sauce', 3)]
     ),
   ];
 

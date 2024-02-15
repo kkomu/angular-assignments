9e1c5cfdbeef355421cd331b2bb2b617e0292e8a
Kykke <kkomulainen@gmail.com>
2024-02-15 14:10:57 +0200

    Example of unsubscribing

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
+  private routeSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -23,8 +25,13 @@ export class UserComponent implements OnInit {
   ngOnInit() {
     this.updateUser(this.route.snapshot.params);
 
-    this.route.params.subscribe((params: Params) => {
+    this.routeSubscription = this.route.params.subscribe((params: Params) => {
       this.updateUser(params);
     });
+  }
+
+  // Angular will automatically handle the subscription, so adding it here for just an example
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
   }
 }

15b806b6d32ebd88bc0e2a1fcd713a7ea1fdcc79
Kykke <kkomulainen@gmail.com>
2024-02-22 13:26:06 +0200

    Add routing with dummy guard

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const { id } = this.route.snapshot.params;
+    this.server = this.serversService.getServer(parseInt(id));
+
+    this.route.params.subscribe((params: Params) => {
+      const { id } = params;
+      this.server = this.serversService.getServer(parseInt(id));
+    });
   }
 
+  onEdit() {
+    //this.router.navigate(['/servers', this.server.id, 'edit']);
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
+    });
+  }
 }

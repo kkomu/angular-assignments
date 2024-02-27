49595070fa850d7ee7780d3f1f6ff19941a355f6
Kykke <kkomulainen@gmail.com>
2024-02-27 14:11:22 +0200

    Add resolver for route

--- routing-start/src/app/app-routing-module.ts
+++ routing-start/src/app/app-routing-module.ts
@@ -7,9 +7,11 @@ import { UserComponent } from './users/user/user.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './servers/server/server.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -25,7 +27,11 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
-      { path: ':id', component: ServerComponent },
+      {
+        path: ':id',
+        component: ServerComponent,
+        resolve: { server: ServerResolver },
+      },
       {
         path: ':id/edit',
         component: EditServerComponent,
@@ -33,9 +39,14 @@ const appRoutes: Routes = [
       },
     ],
   },
+  //   {
+  //     path: 'not-found',
+  //     component: PageNotFoundComponent,
+  //   },
   {
     path: 'not-found',
-    component: PageNotFoundComponent,
+    component: ErrorPageComponent,
+    data: { message: 'Page not found 666!' },
   },
   {
     path: '**',
--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -18,13 +18,17 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const { id } = this.route.snapshot.params;
-    this.server = this.serversService.getServer(parseInt(id));
-
-    this.route.params.subscribe((params: Params) => {
-      const { id } = params;
-      this.server = this.serversService.getServer(parseInt(id));
+    this.route.data.subscribe((data: Data) => {
+      this.server = data['server'];
     });
+
+    // const { id } = this.route.snapshot.params;
+    // this.server = this.serversService.getServer(parseInt(id));
+
+    // this.route.params.subscribe((params: Params) => {
+    //   const { id } = params;
+    //   this.server = this.serversService.getServer(parseInt(id));
+    // });
   }
 
   onEdit() {

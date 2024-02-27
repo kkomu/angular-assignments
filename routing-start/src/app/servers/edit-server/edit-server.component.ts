b6ef695df6f63cd2ddcf50d0e16a15894999233f
Kykke <kkomulainen@gmail.com>
2024-02-27 13:41:20 +0200

    Add guard to prevent navigating away from modified page

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,23 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css'],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
+
+  canDeactivate(): boolean | Promise<boolean> | Observable<boolean> {
+    console.log('CAN DEACTIVATE');
+    if (!this.allowEdit) {
+      return true;
+    }
+
+    if (
+      (this.serverName !== this.server.name ||
+        this.serverStatus !== this.serverStatus) &&
+      !this.changesSaved
+    ) {
+      return confirm("Don't you want to save your changes?");
+    } else {
+      return true;
+    }
+  }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
@@ -41,5 +62,7 @@ export class EditServerComponent implements OnInit {
       name: this.serverName,
       status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 }
